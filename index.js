3fbe4d2140da65954da46d4918d20c868f13dad7
Steve Miller <ben.el.elyon@gmail.com>
2019-05-06 22:07:36 -0400

    Proxy v2 - removed other services, only barebones proxy pointed at other services via bundle.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(__dirname));
+app.use('/:id', express.static(__dirname));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
